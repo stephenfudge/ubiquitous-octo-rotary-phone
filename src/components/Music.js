3c59d92c0566635fecfcca5f9c841afa1078b08a
Stephen Fudge <sfudge@gmail.com>
2022-09-10 12:49:07 -0400

    installed tailwind css and experimenting with different ways to display data

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,80 +1,53 @@
 import React from 'react';
 import music from '../seeds/musicData';
 
-// const music =
-// [{
-//     "title": "AC/DC Family Jewels",
-//     "format": "DVD"
-//   },{
-//     "title": "AC/DC Live At Donnington",
-//     "format": "DVD"
-//   },{
-//     "title": "AC/DC No Bull",
-//     "format": "DVD"
-//   },{
-//     "title": "AC/DC Stiff Upper Lip Live",
-//     "format": "DVD"
-//   },{
-//     "title": "Ben Folds & WASO Live In Perth",
-//     "format": "DVD"
-//   },{
-//     "title": "Billy Talent Scandalous Travellers",
-//     "format": "DVD"
-//   },{
-//     "title": "Black Sabbath Gathered In Their Masses",
-//     "format": "BRD"
-//   },{
-//     "title": "Danko Jones Bring On The Mountain",
-//     "format": "DVD"
-//   },{
-//     "title": "God Bless Ozzy Osbourne",
-//     "format": "BRD"
-//   },{
-//     "title": "Ozzy Osbourne Live At Budokan",
-//     "format": "DVD"
-//   },{
-//     "title": "Red Hot Chili Peppers Off The Map",
-//     "format": "DVD"
-//   },{
-//     "title": "Red Hot Chili Peppers What Hits?",
-//     "format": "DVD"
-//   },{
-//     "title": "S.O.D. Kill Yourself The Movie",
-//     "format": "DVD"
-//   },{
-//     "title": "S.O.D. Speak English Or Live",
-//     "format": "DVD"
-//   },{
-//     "title": "S.O.D. 20 Years Of Destruction",
-//     "format": "DVD"
-//   },{
-//     "title": "Spanky Off Time",
-//     "format": "DVD"
-//   },{
-//     "title": "Steel Panther British Invasion",
-//     "format": "BRD"
-//   },{
-//     "title": "Weird Al Yankovic",
-//     "format": "DVD"
-//   }]
-
   
-  const Music = () => {
+//   const Music = () => {
 
-    return(
-        <div className=''>
-            <div className=''>
-                 {music.map((film) => (
-                    <li className='' key={film.id}>
-                        <div>
-                          <h2 className='film-name'>{[film.title]} {film.format}</h2><br />
-                        </div>
-                    </li>
-    ))}
-            </div>
-        </div>
-    )
+//     return(
+//         <div className=''>
+//             <div className='container bg-gray-300 mx-auto rounded-xl'>
+//                  {music.map((film) => (
+//                     <li className='' key={film.id}>
+//                         <div>
+//                           <h2 className='film-name'>{[film.title]} {film.format}</h2><br />
+//                         </div>
+//                     </li>
+//     ))}
+//             </div>
+//         </div>
+//     )
 
+// }
+
+const Music = () => {
+  return(
+    <div>
+      <table class="table-auto">
+        <thead>
+          <tr>
+            <th>
+              Artist/Title of DVD
+            </th>
+              <th>
+                Media Format
+              </th>
+          </tr>
+        </thead>
+       <tbody>
+          {music.map((film) =>{
+  return (
+    <tr  key={film.id}>
+    <td>{[film.title]}</td>
+    <td>{film.format}</td>
+    </tr>
+          );
+          })}
+         
+      </tbody> 
+     </table>
+    </div>
+  )
 }
 
 export default Music;
