f7313350dc536e2d2fa4802fedca983ab24ae6fc
Stephen Fudge <sfudge@gmail.com>
2022-09-10 13:30:22 -0400

    put films and wrestling into tables

--- src/components/Films.js
+++ src/components/Films.js
@@ -1,22 +1,48 @@
 import React from 'react';
 import films from '../seeds/filmData';
 
-  const Films = () => {
+//   const Films = () => {
 
-    return(
-        <div className=''>
-            <div className=''>
-                 {films.map((film) => (
-                    <li className='' key={film.id}>
-                        <div>
-                          <h2 className='film-name'>{[film.title]} {film.format}</h2><br />
-                        </div>
-                    </li>
-    ))}
-            </div>
-        </div>
-    )
+//     return(
+//         <div className=''>
+//             <div className=''>
+//                  {films.map((film) => (
+//                     <li className='' key={film.id}>
+//                         <div>
+//                           <h2 className='film-name'>{[film.title]} {film.format}</h2><br />
+//                         </div>
+//                     </li>
+//     ))}
+//             </div>
+//         </div>
+//     )
 
+// }
+
+
+const Films = () => {
+  return(
+    <div className=''>
+      <table>
+        <thead>
+          <tr>
+             <th>Title</th>
+               <th>Media Format</th>
+          </tr>
+        </thead>
+        <tbody>
+          {films.map((film) => {
+            return(
+              <tr key={film.id}>
+                <td>{film.title}</td>
+                  <td>{film.format}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default Films;
--- src/components/Wrestling.js
+++ src/components/Wrestling.js
@@ -1,22 +1,52 @@
 import React from 'react';
 import wrestling from '../seeds/wrestlingData';
 
-const Wrestling = () => {
+// const Wrestling = () => {
 
+//     return(
+//         <div className=''>
+//             <div className=''>
+//                  {wrestling.map((film) => (
+//                     <li className='' key={film.id}>
+//                         <div>
+//                           <h2 className='series-name'>{film.promotion} {[film.title]} {film.format} {film.presentation}</h2><br />
+//                         </div>
+//                     </li>
+//     ))}
+//             </div>
+//         </div>
+//     )
+
+// }
+
+
+const Wrestling = () => {
     return(
-        <div className=''>
-            <div className=''>
-                 {wrestling.map((film) => (
-                    <li className='' key={film.id}>
-                        <div>
-                          <h2 className='series-name'>{film.promotion} {[film.title]} {film.format} {film.presentation}</h2><br />
-                        </div>
-                    </li>
-    ))}
-            </div>
+        <div>
+            <table className='table-auto'>
+                <thead>
+                    <tr>
+                        <th>Promotion</th>
+                            <th>Title</th>
+                                <th>Presentation Format</th>
+                                    <th>Media Format</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {wrestling.map((film) => {
+                        return(
+                        <tr key={film.id}>
+                            <td>{film.promotion}</td>
+                                <td>{film.title}</td>
+                                    <td>{film.presentation}</td>
+                                        <td>{film.format}</td>
+                        </tr>
+                    );
+                    })}
+                </tbody>
+            </table>
         </div>
     )
-
 }
 
 export default Wrestling;
