d4687b35ed64e74ed513b49a528b6f5b5c0dd07f
Stephen Fudge <sfudge@gmail.com>
2022-09-17 18:18:16 -0400

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,7 @@ const Footer = () => {
     return (
         <div className='bg-darkpurple p-10'>
          <div id="for-stephen">
-          <div id="first-name">Fudgetown</div>
-        {/* <div id="first-name">Stephen Fudge</div> */}
+        <div id="first-name">Stephen Fudge</div>
         {/* <div id="last-name">Fudge</div> */}
     </div>
 
