d82c12bc9bc95e34fe61c1c1daf2172cd60eeba9
Stephen Fudge <sfudge@gmail.com>
2022-11-17 22:24:36 -0500

    added a few seasons of a tv show

--- src/seeds/tvData.js
+++ src/seeds/tvData.js
@@ -1,274 +1,317 @@
-
-const tv =
-[{
+const tv = [{
     "title": "Boy Meets World",
     "season": 1,
     "format": "DVD"
-  },{
+  }, {
     "title": "Boy Meets World",
     "season": 2,
     "format": "DVD"
   },
-    {
+  {
     "title": "Boy Meets World",
     "season": 3,
     "format": "DVD"
-  },{
+  }, {
     "title": "Boy Meets World",
     "season": 4,
     "format": "DVD"
-  },{
+  }, {
     "title": "Boy Meets World",
     "season": 5,
     "format": "DVD"
-  },{
+  }, {
     "title": "Boy Meets World",
     "season": 6,
     "format": "DVD"
-  },{
+  }, {
     "title": "Boy Meets World",
     "season": 7,
     "format": "DVD"
-  },{
+  }, {
     "title": "Chappelle Show",
     "season": 1,
     "format": "DVD"
-  },{
+  }, {
     "title": "Chappelle Show",
     "season": 2,
     "format": "DVD"
-  },{
+  }, {
     "title": "Chappelle Show",
     "season": 3,
     "format": "DVD"
-  },{
+  }, {
     "title": "Entourage",
     "season": 1,
     "format": "DVD"
-  },{
+  }, {
     "title": "Entourage",
     "season": 2,
     "format": "DVD"
-  },{
+  }, {
     "title": "Entourage",
     "season": 3,
     "format": "DVD"
-  },{
+  }, {
     "title": "Entourage",
     "season": 4,
     "format": "DVD"
-  },{
+  }, {
     "title": "Entourage",
     "season": 5,
     "format": "DVD"
-  },{
+  }, {
     "title": "Entourage",
     "season": 6,
     "format": "DVD"
-  },{
+  }, {
     "title": "Entourage",
     "season": 7,
     "format": "DVD"
-  },{
+  }, {
     "title": "Entourage",
     "season": 8,
     "format": "DVD"
-  },{
+  }, {
     "title": "Franklin & Bash",
     "season": 1,
     "format": "DVD"
-  },{
+  }, {
     "title": "Fresh Prince",
     "season": 3,
     "format": "DVD"
-  },{
+  }, {
     "title": "Fresh Prince",
     "season": 6,
     "format": "DVD"
-  },{
+  }, {
     "title": "How I Met Your Mother",
     "season": 1,
     "format": "DVD"
-  },{
+  }, {
     "title": "How I Met Your Mother",
     "season": 2,
     "format": "DVD"
-  },{
+  }, {
     "title": "How I Met Your Mother",
     "season": 3,
     "format": "DVD"
-  },{
+  }, {
     "title": "How I Met Your Mother",
     "season": 4,
     "format": "DVD"
-  },{
+  }, {
     "title": "How I Met Your Mother",
     "season": 5,
     "format": "DVD"
-  },{
+  }, {
     "title": "How I Met Your Mother",
     "season": 6,
     "format": "DVD"
-  },{
+  }, {
     "title": "How I Met Your Mother",
     "season": 7,
     "format": "DVD"
-  },{
+  }, {
     "title": "How I Met Your Mother",
     "season": 8,
     "format": "DVD"
-  },{
+  }, {
     "title": "How I Met Your Mother",
     "season": 9,
     "format": "DVD"
-  },{
+  }, {
+    "title": "Married With Children",
+    "season": 1,
+    "format": "DVD"
+  }, {
+    "title": "Married With Children",
+    "season": 2,
+    "format": "DVD"
+  }, {
+    "title": "Married With Children",
+    "season": 3,
+    "format": "DVD"
+  }, {
+    "title": "Married With Children",
+    "season": 4,
+    "format": "DVD"
+  }, {
+    "title": "Married With Children",
+    "season": 5,
+    "format": "DVD"
+  }, {
+    "title": "Married With Children",
+    "season": 6,
+    "format": "DVD"
+  }, {
+    "title": "Married With Children",
+    "season": 7,
+    "format": "DVD"
+  }, {
+    "title": "Married With Children",
+    "season": 8,
+    "format": "DVD"
+  }, {
+    "title": "Married With Children",
+    "season": 9,
+    "format": "DVD"
+  }, {
+    "title": "Married With Children",
+    "season": 10,
+    "format": "DVD"
+  }, {
+    "title": "Married With Children",
+    "season": 11,
+    "format": "DVD"
+  }, {
     "title": "Metal Evolution",
     "season": 1,
     "format": "DVD"
-  },{
+  }, {
     "title": "NCIS",
     "season": 1,
     "format": "DVD"
-  },{
+  }, {
     "title": "NCIS",
     "season": 2,
     "format": "DVD"
-  },{
+  }, {
     "title": "NCIS",
     "season": 3,
     "format": "DVD"
-  },{
+  }, {
     "title": "NCIS",
     "season": 4,
     "format": "DVD"
-  },{
+  }, {
     "title": "NCIS",
     "season": 5,
     "format": "DVD"
-  },{
+  }, {
     "title": "NCIS",
     "season": 6,
     "format": "DVD"
-  },{
+  }, {
     "title": "NCIS",
     "season": 7,
     "format": "DVD"
-  },{
+  }, {
     "title": "NCIS",
     "season": 8,
     "format": "DVD"
-  },{
+  }, {
     "title": "NCIS",
     "season": 9,
     "format": "DVD"
-  },{
+  }, {
     "title": "NCIS",
     "season": 10,
     "format": "DVD"
-  },{
+  }, {
     "title": "NCIS",
     "season": 11,
     "format": "DVD"
-  },{
+  }, {
     "title": "NCIS",
     "season": 12,
     "format": "DVD"
-  },{
+  }, {
     "title": "NCIS",
     "season": 13,
     "format": "DVD"
-  },{
+  }, {
     "title": "NCIS",
     "season": 14,
     "format": "DVD"
-  },{
+  }, {
     "title": "Numb3rs",
     "season": 1,
     "format": "DVD"
-  },{
+  }, {
     "title": "Numb3rs",
     "season": 2,
     "format": "DVD"
-  },{
+  }, {
     "title": "Numb3rs",
     "season": 3,
     "format": "DVD"
-  },{
+  }, {
     "title": "Numb3rs",
     "season": 4,
     "format": "DVD"
-  },{
+  }, {
     "title": "Numb3rs",
     "season": 5,
     "format": "DVD"
-  },{
+  }, {
     "title": "Numb3rs",
     "season": 6,
     "format": "DVD"
-  },{
+  }, {
     "title": "Rookie Blue",
     "season": 1,
     "format": "DVD"
-  },{
+  }, {
     "title": "Rookie Blue",
     "season": 2,
     "format": "DVD"
-  },{
+  }, {
     "title": "Rookie Blue",
     "season": 3,
     "format": "DVD"
-  },{
+  }, {
     "title": "Rookie Blue",
     "season": 4,
     "format": "DVD"
-  },{
+  }, {
     "title": "Rookie Blue",
     "season": 5,
     "format": "DVD"
-  },{
+  }, {
     "title": "Saved By The Bell",
     "season": 1,
     "format": "DVD"
-  },{
+  }, {
     "title": "Saved By The Bell",
     "season": 2,
     "format": "DVD"
-  },{
+  }, {
     "title": "Saved By The Bell",
     "season": 3,
     "format": "DVD"
-  },{
+  }, {
     "title": "Saved By The Bell",
     "season": 4,
     "format": "DVD"
-  },{
+  }, {
     "title": "Silicon Valley",
     "season": 1,
     "format": "DVD"
-  },{
+  }, {
     "title": "Silicon Valley",
     "season": 2,
     "format": "DVD"
-  },{
+  }, {
     "title": "Silicon Valley",
     "season": 3,
     "format": "DVD"
-  },{
+  }, {
     "title": "Silicon Valley",
     "season": 4,
     "format": "DVD"
-  },{
+  }, {
     "title": "Two Guys And A Girl",
     "season": 1,
     "format": "DVD"
-  },{
+  }, {
     "title": "Two Guys And A Girl",
     "season": 2,
     "format": "DVD"
-  },{
+  }, {
     "title": "Two Guys And A Girl",
     "season": 3,
     "format": "DVD"
-  }]
+  }
+]
 
-  export default tv;
+export default tv;
