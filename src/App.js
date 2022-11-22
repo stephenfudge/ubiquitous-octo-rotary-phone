203f5cef551d1f5289d0289c84c347315b97b1ee
Stephen Fudge <sfudge@gmail.com>
2022-11-22 17:08:39 -0500

    added some filtered options

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
-
-import './App.css';
+import "./App.css";
 // import Main from './pages/Main';
-import NavRouter from './components/NavRouter';
-import Footer from './components/Footer';
-import TestMain from './pages/TestMain';
-import Films from './components/Films';
-import Tv from './components/Tv';
-import Music from './components/Music';
-import Wrestling from './components/Wrestling';
-import FilmsDVD from './components/Films/FilmsDVD'
-import FilmsBRD from './components/Films/FilmsBRD';
+import NavRouter from "./components/NavRouter";
+import Footer from "./components/Footer";
+import TestMain from "./pages/TestMain";
+import Tv from "./components/Tv";
+import Music from "./components/Music";
+
+import Films from "./components/Films";
+import FilmsDVD from "./components/Films/FilmsDVD";
+import FilmsBRD from "./components/Films/FilmsBRD";
+
+// Wrestling
+import Wrestling from "./components/Wrestling";
+import WWE from "./components/Wrestling/WWE";
+import Compilation from './components/Wrestling/Presentation/Compilation'
+import Documentary from "./components/Wrestling/Presentation/Documentary";
+import PPV from "./components/Wrestling/Presentation/PPV";
+
 
 // function App() {
 //   return (
@@ -27,68 +29,31 @@ import FilmsBRD from './components/Films/FilmsBRD';
 //   );
 // }
 
-
-
 function App() {
-  return ( <
-    Router >
-    <
-    div className = 'App' >
-    <
-    NavRouter / >
-    <
-    Routes >
+  return (
+    <Router>
+      <div className="App">
+        <NavRouter />
+        <Routes>
+          <Route path="/" element={<TestMain />} />
 
-    <
-    Route path = '/'
-    element = {
-      < TestMain / >
-    }
-    />
+          <Route path="/films" element={<Films />} />
+          <Route path="/films/dvd" element={<FilmsDVD />} />
+          <Route path="/films/brd" element={<FilmsBRD />} />
 
-    <
-    Route path = '/films'
-    element = {
-      < Films / >
-    }
-    /> <
-    Route path = '/music'
-    element = {
-      < Music / >
-    }
-    /> <
-    Route path = '/tv'
-    element = {
-      < Tv / >
-    }
-    /> <
-    Route path = '/wrestling'
-    element = {
-      < Wrestling / >
-    }
-    /> <
-    Route path = '/films/dvd'
-    element = {
-      < FilmsDVD / >
-    }
-    /> <
-    Route path = '/films/brd'
-    element = {
-      < FilmsBRD / >
-    }
-    />
+          <Route path="/music" element={<Music />} />
 
-    <
-    /Routes>
+          <Route path="/tv" element={<Tv />} />
 
+          <Route path="/wrestling" element={<Wrestling />} />
+          <Route path="/wrestling/wwe" element={<WWE />} />
+          <Route path="/wrestling/compilation" element={<Compilation />} />
+        </Routes>
 
-    <
-    Footer / >
-    <
-    /div> <
-    /Router>
-  )
+        <Footer />
+      </div>
+    </Router>
+  );
 }
-
 
 export default App;
